12833b612d5ad4898cbcd537f9c0879f609e2397
Nemanja Balac <nbposta@gmail.com>
2018-12-15 17:47:30 +0100

    About page content update

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,14 @@ const About = () => {
   return (
     <div>
       <h1 className="h-1">About</h1>
-      <p>Primis eu accumsan diam orci non pharetra, ad et amet enim pretium gravida pharetra, metus scelerisque nunc posuere cubilia venenatis sodales ac varius adipiscing congue euismod scelerisque sagittis volutpat ipsum bibendum massa pulvinar non, odio magna at pellentesque curabitur convallis nec rhoncus sollicitudin morbi lacinia augue.</p>
-      <p>Primis eu accumsan diam orci non pharetra, ad et amet enim pretium gravida pharetra, metus scelerisque nunc posuere cubilia venenatis.</p>
+      <p>This small React app is a good place to see practical use of the create-react-app and can be a good starting point for creating larger apps. Find out about the directory structure and a fine way to organize your files. Versatile React ES6 techniques has been used together with:</p>
+      <ul>
+        <li>ES6</li>
+        <li>JS fetch API</li>
+        <li>Scss</li>
+        <li>Bootstrap 4</li>
+      </ul>
+      <p className="msg warning">This App is not styled for the Inetnet Explorer and never will be.</p>
     </div>
   )
 }
